bdc86477bc607bcc81248f3a3edfaccdbe4d5178
Jhon Trujillo Serrano <jhontruse@gmail.com>
2026-05-25 23:03:36 -0500

    avance de create

--- src/main/java/com/jhontruse/wsr_task/model/records/CreateTaskRequest.java
+++ src/main/java/com/jhontruse/wsr_task/model/records/CreateTaskRequest.java
@@ -3,17 +3,43 @@ package com.jhontruse.wsr_task.model.records;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jhontruse.wsr_task.model.enums.EstadoTask;
 import com.jhontruse.wsr_task.model.enums.PrioridadTask;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 
 public record CreateTaskRequest(
+
+        @NotBlank(message = "El título del task es obligatorio.")
+        @Size(min = 3, max = 250, message = "El título debe tener entre 3 y 250 caracteres.")
+        @Column("TITULO")
+        @Schema(description = "Título de task", example = "Tarea de física")
         String titulo,
+
+        @Size(min = 3, max = 800, message = "La descripción debe tener entre 3 y 800 caracteres.")
+        @Column("DESCRIPCION")
+        @Schema(description = "Descripcion de task", example = "Tarea de física")
         String descripcion,
+
+        @NotBlank(message = "El estado del task es obligatorio.")
+        @Size(min = 1, max = 20, message = "El estado debe tener entre 3 y 20 caracteres.")
+        @Column("ESTADO")
+        @Schema(description = "Estado de task", example = "PROGRESO")
         EstadoTask estado,
+
+        @NotBlank(message = "La prioridad del task es obligatorio.")
+        @Size(min = 1, max = 20, message = "La prioridad debe tener entre 3 y 20 caracteres.")
+        @Column("PRIORIDAD")
+        @Schema(description = "Prioridad de task", example = "MEDIO")
         PrioridadTask prioridad,
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+
+        @Column("FEC_REGISTRO")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Lima") @Schema(description = "Fecha de registro del task", example = "2026-04-27 10:00:00")
         LocalDateTime fecRegistro,
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime fecVencimiento
-) {
+
+        @Column("FEC_VENCIMIENTO")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Lima") @Schema(description = "Fecha de vencimiento del task", example = "2026-04-27 10:00:00")
+        LocalDateTime fecVencimiento) {
 }
--- src/main/java/com/jhontruse/wsr_task/model/records/GenericRespondePage.java
+++ src/main/java/com/jhontruse/wsr_task/model/records/GenericRespondePage.java
@@ -1,5 +1,10 @@
 package com.jhontruse.wsr_task.model.records;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.relational.core.mapping.Column;
+
 import java.util.List;
 
 public record GenericRespondePage<T>(
